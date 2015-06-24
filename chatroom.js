df7737cb6adf857bd50c88421b30354b90818fb1
phil8217 <a82061617@hmail.com>
2015-06-25 03:52:04 +0800

    commit1

--- chatroom.js
+++ chatroom.js
@@ -1,40 +1,38 @@
-var app = require('express')();
+var app = require('express')(); 
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http);//導入需要的函式酷
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/chatroom.html');
+app.get('/', function(req, res){//app.get指定訪問路徑對應的回調函數(routing)req,存放請求的所有資訊;res回應請求
+	res.sendFile(__dirname + '/chatroom.html');//讀取chatroom.html
 });
 
-var user_count = 0;
 
-
-io.on('connection', function(socket){
+io.on('connection', function(socket){//啟動socket.io並連練
 
 	
-	socket.on('add user',function(msg){
+	socket.on('add user',function(msg){//等待觸發add user事件
 		socket.username = msg;
-		console.log("new user:"+msg+" logged.");
-		io.emit('add user',{
+		console.log("歡迎"+msg+"加入");
+		io.emit('add user',{//傳給CLIENT端add user
 			username: socket.username
 		});
 	});
 
 	
-	socket.on('chat message', function(msg){
+	socket.on('chat message', function(msg){//等待觸發chat message事件
 
 		console.log(socket.username+":"+msg);
 
   		
-		io.emit('chat message', {
+		io.emit('chat message', {//回傳CLIENT chat massage
 			username:socket.username,
 			msg:msg
 		});
 	});
 
-	socket.on('disconnect',function(){
-		console.log(socket.username+" left.");
-		io.emit('user left',{
+	socket.on('disconnect',function(){//等待出發disconnect事件
+		console.log(socket.username+" 離開了");
+		io.emit('user left',{//回傳CLIENT端user left
 			username:socket.username
 		});
 	});
